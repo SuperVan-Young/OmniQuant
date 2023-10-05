f9dc248d22c7e39c290ce76ec34973dbd4e1e055
Chenhao Xue <xch927027@pku.edu.cn>
2023-10-05 15:44:56 +0800

    debug post layernorm script

--- scripts/aow/post_layernorm_outlier.sh
+++ scripts/aow/post_layernorm_outlier.sh
@@ -1,6 +1,8 @@
 # MODEL_DIR=/home/zhangchen/hugginface
 MODEL_DIR=/home/xuechenhao/huggingface
 
+# This script is tailored for running on 6 V100 GPUs
+
 # Currently by default, aowquant doesn't quantize weight and activation unless specified!
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --model  $MODEL_DIR/llama-7b-hf-transformers-4.29 --eval_ppl \
@@ -44,35 +46,37 @@ CUDA_VISIBLE_DEVICES=5 python main.py \
 
 wait
 
-CUDA_VISIBLE_DEVICES=0 python main.py \
---model  $MODEL_DIR/llama-13b-hf-transformers-4.29 --eval_ppl \
---epoch 0 --output_dir ./log/llama-13b-aow-qkv-fc1 \
---quant-method aowquant \
---wbits 4 --abits 4 &
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
---model  $MODEL_DIR/llama-13b-hf-transformers-4.29 --eval_ppl \
---epoch 0 --output_dir ./log/llama-13b-aow-qkv-fc1 \
---quant-method aowquant \
---wbits 4 --abits 4 \
---aow-quant-act-qkv &
-
 CUDA_VISIBLE_DEVICES="2,3" python main.py \
 --model  $MODEL_DIR/llama-13b-hf-transformers-4.29 --eval_ppl \
---epoch 0 --output_dir ./log/llama-13b-aow-qkv-fc1 \
+--epoch 0 --output_dir ./log/llama-13b-aow \
+--quant-method aowquant \
+--multigpu \
+--wbits 4 --abits 4 &
+
+CUDA_VISIBLE_DEVICES="4,5" python main.py \
+--model  $MODEL_DIR/llama-13b-hf-transformers-4.29 --eval_ppl \
+--epoch 0 --output_dir ./log/llama-13b-aow-qkv \
+--quant-method aowquant \
+--wbits 4 --abits 4 \
+--multigpu \
+--aow-quant-act-qkv &
+
+CUDA_VISIBLE_DEVICES="6,7" python main.py \
+--model  $MODEL_DIR/llama-13b-hf-transformers-4.29 --eval_ppl \
+--epoch 0 --output_dir ./log/llama-13b-aow-fc1 \
 --quant-method aowquant \
 --wbits 4 --abits 4 \
 --multigpu \
 --aow-quant-act-fc1 &
 
-CUDA_VISIBLE_DEVICES="4,5" python main.py \
+CUDA_VISIBLE_DEVICES="2,3" python main.py \
 --model  $MODEL_DIR/opt-13b --eval_ppl \
 --epoch 0 --output_dir ./log/opt-13b-aow \
 --quant-method aowquant \
 --multigpu \
 --wbits 4 --abits 4 &
 
-CUDA_VISIBLE_DEVICES="6,7" python main.py \
+CUDA_VISIBLE_DEVICES="4,5" python main.py \
 --model  $MODEL_DIR/opt-13b --eval_ppl \
 --epoch 0 --output_dir ./log/opt-13b-aow-qkv \
 --quant-method aowquant \
@@ -80,7 +84,7 @@ CUDA_VISIBLE_DEVICES="6,7" python main.py \
 --multigpu \
 --aow-quant-act-qkv &
 
-CUDA_VISIBLE_DEVICES="0,1" python main.py \
+CUDA_VISIBLE_DEVICES="6,7" python main.py \
 --model  $MODEL_DIR/opt-13b --eval_ppl \
 --epoch 0 --output_dir ./log/opt-13b-aow-fc1 \
 --quant-method aowquant \
