bb93fda4e3dcfd9ce4d5323b47616aedd071f4c8
Chenhao Xue <xch927027@pku.edu.cn>
2023-10-06 16:36:26 +0800

    automatic parse logs

--- parse_log.py
+++ parse_log.py
@@ -1,33 +1,106 @@
 import os
 import pandas as pd
 
-df = pd.DataFrame(columns=['test-name', 'wikitext2', 'ptb', 'c4', 'ptb-new', 'c4-new'])
+os.makedirs("./results", exist_ok=True)
 
-# listdir log
-for subdir in os.listdir('./log'):
-    logs = os.listdir('./log/' + subdir)
-    logs = sorted(logs)
-    recent_log = logs[-1]
-    all_results = {}
-    
-    with open(os.path.join('./log', subdir, recent_log), 'r') as f:
-        # iterate through log lines
+def collect_results() -> pd.DataFrame:
 
-        for line in f:
-            if "(main.py 147): INFO" in line:
-                # use regex to extract str after INFO
-                result = line.split("INFO")[1]
-                dataset, ppl = result.split(":")
-                dataset = dataset.strip()
-                ppl = float(f"{float(ppl):.2f}")
-                all_results[dataset] = ppl
+    df = pd.DataFrame(columns=['experiment', 'model', 'wikitext2', 'ptb', 'c4', 'ptb-new', 'c4-new'])
 
-    # print(subdir, " ".join([f"{ppl}" for ppl in all_results.values()]))
-    
-    # save to df
-    all_results['test-name'] = subdir
-    print(all_results)
-    df.loc[len(df)] = all_results
+    output_dir = './output'
 
-with open('log.csv', 'w') as f:
-    df.to_csv(f, index=False)
+    # listdir log
+    for experiment in os.listdir(output_dir):
+        models = os.listdir(os.path.join(output_dir, experiment))
+        models = sorted(models)
+
+        for model in models:
+            logs = os.listdir(os.path.join(output_dir, experiment, model))
+            log = sorted(logs)[-1]
+        
+            with open(os.path.join(output_dir, experiment, model, log), 'r') as f:
+                # iterate through log lines
+
+                result = {
+                    'experiment': experiment, 
+                    'model': model,
+                }
+
+                for line in f:
+                    if "(main.py 148): INFO" in line:
+                        # use regex to extract str after INFO
+                        cur_result = line.split("INFO")[1]
+                        dataset, ppl = cur_result.split(":")
+                        dataset = dataset.strip()
+                        ppl = float(f"{float(ppl):.2f}")
+                        result[dataset] = ppl
+
+            # print(subdir, " ".join([f"{ppl}" for ppl in all_results.values()]))
+            
+            # save to df
+            df.loc[len(df)] = result
+
+    return df
+
+def lookup_results(
+    df, 
+    experiment_list, 
+    model_list,
+    save_path=None,
+):
+    """
+    Lookup results from df.
+    """
+    # set index to experiment and model
+    df = df.set_index(['experiment', 'model'])
+
+    # filter by experiment and model
+    mask = (df.index.get_level_values('experiment').isin(experiment_list)) & (df.index.get_level_values('model').isin(model_list))
+    filtered_df = df.loc[mask]
+
+    sorted_df = filtered_df.sort_values(['model', 'experiment'])
+
+    if save_path:
+        with open(save_path, 'w') as f:
+            sorted_df.to_csv(f, index=True)
+
+    return sorted_df
+
+MODEL_LIST = ['opt-6.7b', 'opt-13b', 'llama-7b-hf-transformers-4.29', 'llama-13b-hf-transformers-4.29']
+
+LOOKUPS = [
+    {
+        'experiment_list': ['W16A16', 'qkvproj_W16A4', 'qkvproj_W16A8', 'qkvproj_W16A4_ol0.01'],
+        'model_list': MODEL_LIST,
+        'save_path': 'results/qkvproj.csv'
+    },
+    {
+        'experiment_list': ['W16A16', 'oproj_W16A4', 'oproj_W16A8'],
+        'model_list': MODEL_LIST,
+        'save_path': 'results/oproj.csv'
+    },
+    {
+        'experiment_list': ['W16A16', 'fc1_W16A4', 'fc1_W16A8', 'fc1_W16A4_ol0.01'],
+        'model_list': MODEL_LIST,
+        'save_path': 'results/fc1.csv'
+    },
+    {
+        'experiment_list': ['W16A16', 'fc2_W16A4', 'fc2_W16A8'],
+        'model_list': MODEL_LIST,
+        'save_path': 'results/fc2.csv'
+    },
+]
+
+if __name__ == '__main__':
+    df = collect_results()
+    # with open('log.csv', 'w') as f:
+    #     df.to_csv(f, index=False)
+
+    for lookup in LOOKUPS:
+        lookup_results(
+            df, 
+            experiment_list=lookup['experiment_list'], 
+            model_list=lookup['model_list'],
+            save_path=lookup['save_path'],
+        )
+
