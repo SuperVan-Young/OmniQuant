a64756ae73028a741a6fb6f4c353d05f7b94457e
Chenhao Xue <xch927027@pku.edu.cn>
2023-10-19 08:41:57 +0800

    config generation

---
+++ experiments/configs.py
@@ -0,0 +1,89 @@
+# Experiment Configurations
+
+from itertools import product
+
+def get_outlier_name(outlier_ratio):
+    ol_name = f"1p{int(1/outlier_ratio)}"
+    return ol_name
+
+def get_baseline_experiment_configs():
+    """
+    Baseline experiments
+    """
+    config_dict = {}
+
+    config_dict['W16A16'] = {
+        "wbits": 16,
+        "abits": 16,
+    }
+
+    layer_type_list = [
+        'qkvproj',
+        'fc1',
+        'oproj',
+        'fc2',
+        'q',
+        'k',
+        'v',
+    ]
+    abits_list = [4, 8]
+    for layer_type, abits in product(layer_type_list, abits_list):
+        config_name = f"{layer_type}_W16A{abits}"
+        config = {
+            'wbits': 16,
+            'abits': abits,
+            f"aow-quant-act-{layer_type}": None,
+        }
+        config_dict[config_name] = config
+
+    return config_dict
+
+def add_outlier_experiment_configs():
+    config_dict = {}
+
+    outlier_ratio_list = [
+        1 / 128,
+        1 / 64,
+        1 / 32,
+        1 / 16,
+    ]
+    layer_type_list = [
+        'qkvproj',
+        'fc1',
+        'oproj',
+        'fc2',
+    ] # ignore matmuls
+    for layer_type, outlier_ratio in product(layer_type_list, outlier_ratio_list):
+        ol_name = get_outlier_name(outlier_ratio)
+        config_name = f"{layer_type}_W16A4_ol{ol_name}"
+        config = {
+            'wbits': 16,
+            'abits': 4,
+            f"aow-quant-act-{layer_type}": None,
+            'act-outlier-ratio': outlier_ratio,
+        }
+        config_dict[config_name] = config
+
+    return config_dict
+
+
+def add_grouping_experiment_configs():
+    config_dict = {}
+    
+    layer_type_list = [
+        'qkvproj',
+        'fc1',
+        'oproj',
+        'fc2',
+    ] # ignore matmuls
+    for layer_type in layer_type_list:
+        config_name = f"{layer_type}_W16A4_g128"
+        config = {
+            'wbits': 16,
+            'abits': 4,
+            f"aow-quant-act-{layer_type}": None,
+            'act-group-size': 128,
+        }
+        config_dict[config_name] = config
+
+    return config_dict
