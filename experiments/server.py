551ce8453108f3b6858f57bea78f06c720b40ca9
Chenhao Xue <xch927027@pku.edu.cn>
2023-10-19 09:16:28 +0800

    server allocate gpu

--- experiments/server.py
+++ experiments/server.py
@@ -7,13 +7,30 @@ class ServerConfig():
         self.gpu_list = kwargs.get('gpu_list')
         self.gpu_memory = kwargs.get('gpu_memory')
         self.model_dir = kwargs.get('model_dir')
+        
+        self._used_gpu = 0
 
-    def allocate_gpu(self, model_name):
+    def _allocate_num_gpu(self, model_name) -> int:
         weight_size_in_GB = float(model_name.split("-")[1].replace("b", ""))
         required_size_in_GB = weight_size_in_GB * 2.5
         num_gpu = math.ceil(required_size_in_GB / self.gpu_memory)
         assert num_gpu <= len(self.gpu_list), "not enough gpu"
         return num_gpu
+    
+    def allocate_gpu_ids(self, model_name) -> tuple(list, bool):
+        """
+        Allocate gpu ids for model, return (gpu_ids, is_full)
+        """
+        num_gpu = self._allocate_num_gpu(model_name)
+        if self._used_gpu + num_gpu > len(self.gpu_list):
+            # GPU is full for this round, start next round
+            self._used_gpu = num_gpu
+            gpu_ids = self.gpu_list[:self._used_gpu]
+            return gpu_ids, True
+        else:
+            gpu_ids = self.gpu_list[self._used_gpu:self._used_gpu + num_gpu]
+            self._used_gpu += num_gpu
+            return gpu_ids, False
     
 V100_SERVER_CONFIG = {
     'gpu_list': [2, 3, 4, 5, 6, 7],
