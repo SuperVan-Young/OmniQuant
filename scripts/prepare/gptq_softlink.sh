29c1eda41226f3a38915c048cf7944db76b3929e
Chenhao Xue <xch927027@pku.edu.cn>
2023-10-22 16:25:54 +0800

    softlink for gptq checkpoint

---
+++ scripts/prepare/gptq_softlink.sh
@@ -0,0 +1,15 @@
+ln -s /datasets/gptq/hugginface/llama-13b-meta-4bit /home/xuechenhao/hugginface/llama-13b-meta-4bit
+ln -s /datasets/gptq/hugginface/llama-30b-meta-4bit /home/xuechenhao/hugginface/llama-30b-meta-4bit
+ln -s /datasets/gptq/hugginface/llama-65b-meta-4bit /home/xuechenhao/hugginface/llama-65b-meta-4bit
+
+ln -s /datasets/gptq/hugginface/opt-13b-4bit /home/xuechenhao/hugginface/opt-13b-4bit
+ln -s /datasets/gptq/hugginface/opt-30b-4bit /home/xuechenhao/hugginface/opt-30b-4bit
+ln -s /datasets/gptq/hugginface/opt-66b-4bit /home/xuechenhao/hugginface/opt-66b-4bit
+
+ln -s /datasets/gptq/hugginface/llama-13b-meta-4bit-128g /home/xuechenhao/hugginface/llama-13b-meta-4bit-128g
+ln -s /datasets/gptq/hugginface/llama-30b-meta-4bit-128g /home/xuechenhao/hugginface/llama-30b-meta-4bit-128g
+ln -s /datasets/gptq/hugginface/llama-65b-meta-4bit-128g /home/xuechenhao/hugginface/llama-65b-meta-4bit-128g
+
+ln -s /datasets/gptq/hugginface/opt-13b-4bit-128g /home/xuechenhao/hugginface/opt-13b-4bit-128g
+ln -s /datasets/gptq/hugginface/opt-30b-4bit-128g /home/xuechenhao/hugginface/opt-30b-4bit-128g
+ln -s /datasets/gptq/hugginface/opt-66b-4bit-128g /home/xuechenhao/hugginface/opt-66b-4bit-128g
